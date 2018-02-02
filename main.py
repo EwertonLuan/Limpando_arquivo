ed6e42641f9ee76c16c9ac8e8de93cde0f321f72
Ewerton <ewerton.luan@outlook.com>
2018-02-02 00:08:38 -0200

    Correção de letra

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from documento import limpando
 
 
 limpando(input('Digite o nome do arquivo para limpeza: '),
-         input('Digite o nome da saido do arquivo: '),
+         input('Digite o nome de saida do arquivo: '),
          input('Item para procura: '))
